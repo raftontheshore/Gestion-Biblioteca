c80848b4904c572ab1e8100cb2ed08e40b9bad6d
Waldemar <waldemariturrieta.dev@gmail.com>
2025-11-06 00:25:14 -0300

    Layout De AgregarSocios

--- Menu.java
+++ Menu.java
@@ -35,7 +35,21 @@ public class Menu extends JFrame {
     private JTextArea jtaTextoInicio;
     private Titulos jlTituloSocio;
     private Titulos jlTituloPrestamo;
-    //  
+
+    // Prueba Agregar Socio
+    private MiPanelEscritorio jifAgregarSocio;
+    private JRadioButton rbDocente;
+    private JRadioButton rbAlumno;
+    private ButtonGroup bgTipoSocio;
+    private JLabel jlDNI;
+    private JTextField jtfDNI;
+    private JLabel jlNombre;
+    private JTextField jtfNombre;
+    private JLabel jlDiasPrestamo;
+    private JTextField jtfDiasPrestamo;
+    private JLabel jlCarreraAsignatura; 
+    private JTextField jtfCarreraAsignatura;
+    private MiBoton jbGuardarSocio; 
 
     public Menu() {
         iniciarComponentes();
@@ -84,6 +98,9 @@ public class Menu extends JFrame {
         this.jlTitulo.setForeground(new Color(100, 100, 100));//Settea el color de la fuente
         this.jlTitulo.setText("Biblioteca Central");
 
+        // PANEL AGREGAR SOCIOS LLAMADA
+        configurarPanelAgregarSocio();
+
         //Settea el Layout de los paneles a null para ubicar con presicion los componente con coordenadas (x, y)
         this.jpCabecera.setLayout(null);
         this.jpOpciones.setLayout(null);
@@ -107,7 +124,7 @@ public class Menu extends JFrame {
         //Añade icono de listar socios
         this.jifSocio.add(jlListarSocios);
         this.jlListarSocios.setBounds(460, 100, 64, 64);
-        
+
         //Añade icono de agregar prestamo
         this.jifPrestamo.add(jlAgregarPrestamo);
         this.jlAgregarPrestamo.setBounds(60, 100, 64, 64);
@@ -131,6 +148,10 @@ public class Menu extends JFrame {
                 public void mouseExited(MouseEvent evt) {
                     jbAgregarSocio.jBotonMouseExited(evt);
                 }
+
+                public void mouseClicked(MouseEvent evt) {
+                    jtpEscritorio.setSelectedIndex(3); 
+                }
             });
 
         //Settea los atributos del texto en el panel interno Inicio     
@@ -143,10 +164,8 @@ public class Menu extends JFrame {
 
         //Añade el titulo a la pestaña de socios
         this.jifSocio.add(jlTituloSocio);
-        
-        //Añade el titulo a la pestaña de prestamos
         this.jifPrestamo.add(jlTituloPrestamo);
-        
+
         //Añade botones a la pestaña de prestamos
         this.jifPrestamo.add(jbAgregarPrestamo);
         this.jbAgregarPrestamo.setBounds(25, 200, 130, 40);
@@ -181,7 +200,7 @@ public class Menu extends JFrame {
                     jbListarPrestamos.jBotonMouseExited(evt);
                 }
             });    
-        
+
         //Color de fondo para el panel de opciones
         this.jpOpciones.setBackground(new Color(102, 102, 102));
 
@@ -234,7 +253,7 @@ public class Menu extends JFrame {
                     jbOpcionLibros.jbOpcionMouseExited(evt);
                 }
             });
-            
+
         //Inicializa el texto de la opcion prestamos y añade el boton al panel de opciones
         this.jbOpcionPrestamos.setText("Prestamos");
         this.jpOpciones.add(jbOpcionPrestamos);
@@ -247,7 +266,7 @@ public class Menu extends JFrame {
                 public void mouseExited(MouseEvent evt) {
                     jbOpcionPrestamos.jbOpcionMouseExited(evt);
                 }
-                
+
                 public void mouseClicked(MouseEvent evt){
                     jifPrestamoMouseClicked(evt);
                 }
@@ -259,6 +278,7 @@ public class Menu extends JFrame {
         this.jtpEscritorio.addTab("tab1", jifInicio);// Agrega una ventana interna al panel de contenido
         this.jtpEscritorio.addTab("tab2", jifSocio);
         this.jtpEscritorio.addTab("tab3", jifPrestamo);
+        this.jtpEscritorio.addTab("tab4", jifAgregarSocio); 
         //Añade el panel de opciones a la ventana principal
         this.getContentPane().add(jpOpciones);
         this.jpOpciones.setBounds(0, 90, 170, 420);
@@ -277,8 +297,170 @@ public class Menu extends JFrame {
     private void jifSocioMouseClicked(MouseEvent evt) {                                           
         this.jtpEscritorio.setSelectedIndex(1);
     }
-    
+
     private void jifPrestamoMouseClicked(MouseEvent evt) {                                           
         this.jtpEscritorio.setSelectedIndex(2);
     }
+
+    private void jifAgregarSocioMouseClicked(MouseEvent evt) {                                           
+        this.jtpEscritorio.setSelectedIndex(3);
+    }
+
+    /**
+     * Crea, inicializa y posiciona todos los componentes 
+     * para el panel de "Agregar Socio" (jifAgregarSocio).
+     * (VERSION 4 - Componentes movidos a la izquierda)
+     */
+    private void configurarPanelAgregarSocio() {
+
+        // 1. Crear el Panel Principal
+        jifAgregarSocio = new MiPanelEscritorio("/imagenes/Fondo_Biblioteca_Escritorio_Blur.jpeg");
+        jifAgregarSocio.setLayout(null); 
+
+        // --- Definir Fuentes y Colores ---
+        Font fontTitulo = new Font("Corbel", Font.BOLD, 24);
+        Font fontLabel = new Font("Corbel", Font.BOLD, 14);
+        Font fontRadio = new Font("Corbel", Font.PLAIN, 14);
+        Color colorTexto = Color.WHITE; 
+
+        // 2. Definir Geometría del Panel
+        int panelWidth = 730; 
+
+        // Desplazamiento (Valor Auxiliar Usado Para Centrar Todos Los Elementos, en este caso a la izquierda)
+        int desplazamiento = 40; 
+
+        // 3. Crear y posicionar los componentes
+
+        // --- Título ---
+        JLabel jlTituloForm = new JLabel("Agregar Nuevo Socio");
+        jlTituloForm.setFont(fontTitulo);
+        jlTituloForm.setForeground(colorTexto);
+        int tituloWidth = 300; 
+        // Calculamos el centro y LUEGO restamos el desplazamiento
+        int tituloX = ((panelWidth - tituloWidth) / 2); 
+        jlTituloForm.setBounds(tituloX, 20, tituloWidth, 30);
+        jifAgregarSocio.add(jlTituloForm);
+
+        // --- Radio Buttons ---
+        int radioDocenteWidth = 100;
+        int radioAlumnoWidth = 100;
+        int radioGap = 10;
+        int radioBlockWidth = radioDocenteWidth + radioGap + radioAlumnoWidth; 
+        int radioStartX = ((panelWidth - radioBlockWidth) / 2) - desplazamiento;
+
+        rbDocente = new JRadioButton("Docente");
+        rbDocente.setFont(fontRadio);
+        rbDocente.setForeground(colorTexto);
+        rbDocente.setOpaque(false);
+        rbDocente.setBounds(radioStartX, 70, radioDocenteWidth, 30);
+        jifAgregarSocio.add(rbDocente);
+
+        rbAlumno = new JRadioButton("Alumno");
+        rbAlumno.setFont(fontRadio);
+        rbAlumno.setForeground(colorTexto);
+        rbAlumno.setOpaque(false);
+        rbAlumno.setSelected(true); 
+        rbAlumno.setBounds(radioStartX + radioDocenteWidth + radioGap, 70, radioAlumnoWidth, 30);
+        jifAgregarSocio.add(rbAlumno);
+
+        bgTipoSocio = new ButtonGroup();
+        bgTipoSocio.add(rbDocente);
+        bgTipoSocio.add(rbAlumno);
+
+        // --- Columnas de Campos ---
+        int label_ancho = 200;
+        int field_ancho = 220; 
+        int field_alto = 28;    
+        int column_gap = 60;    
+
+        int formBlockWidth = field_ancho + column_gap + field_ancho;
+        int col1_x = ((panelWidth - formBlockWidth) / 2) - desplazamiento;
+        int col2_x = col1_x + field_ancho + column_gap; 
+
+        // --- Columna Izquierda (DNI y Nombre) ---
+        jlDNI = new JLabel("DNI:");
+        jlDNI.setFont(fontLabel);
+        jlDNI.setForeground(colorTexto);
+        jlDNI.setBounds(col1_x, 120, label_ancho, 25);
+        jifAgregarSocio.add(jlDNI);
+
+        jtfDNI = new JTextField();
+        jtfDNI.setBounds(col1_x, 150, field_ancho, field_alto);
+        jifAgregarSocio.add(jtfDNI);
+
+        // Nombre
+        jlNombre = new JLabel("Nombre Completo:");
+        jlNombre.setFont(fontLabel);
+        jlNombre.setForeground(colorTexto);
+        jlNombre.setBounds(col1_x, 190, label_ancho, 25);
+        jifAgregarSocio.add(jlNombre);
+
+        jtfNombre = new JTextField();
+        jtfNombre.setBounds(col1_x, 220, field_ancho, field_alto);
+        jifAgregarSocio.add(jtfNombre);
+
+        // --- Columna Derecha (Carrera y Días) ---
+        jlCarreraAsignatura = new JLabel("Carrera:"); 
+        jlCarreraAsignatura.setFont(fontLabel);
+        jlCarreraAsignatura.setForeground(colorTexto);
+        jlCarreraAsignatura.setBounds(col2_x, 120, label_ancho, 25);
+        jifAgregarSocio.add(jlCarreraAsignatura);
+
+        jtfCarreraAsignatura = new JTextField();
+        jtfCarreraAsignatura.setBounds(col2_x, 150, field_ancho, field_alto);
+        jifAgregarSocio.add(jtfCarreraAsignatura);
+
+        // Días de Préstamo
+        jlDiasPrestamo = new JLabel("Días de Préstamo:");
+        jlDiasPrestamo.setFont(fontLabel);
+        jlDiasPrestamo.setForeground(colorTexto);
+        jlDiasPrestamo.setBounds(col2_x, 190, label_ancho, 25);
+        jifAgregarSocio.add(jlDiasPrestamo);
+
+        jtfDiasPrestamo = new JTextField("20"); 
+        jtfDiasPrestamo.setEnabled(false); 
+        jtfDiasPrestamo.setBounds(col2_x, 220, field_ancho, field_alto);
+        jifAgregarSocio.add(jtfDiasPrestamo);
+
+        // --- Botón Guardar ---
+        int botonWidth = 150;
+        int botonHeight = 40;
+        int botonX = ((panelWidth - botonWidth) / 2) - desplazamiento;
+        jbGuardarSocio = new MiBoton("Guardar Socio");
+        jbGuardarSocio.setBounds(botonX, 300, botonWidth, botonHeight); 
+        jifAgregarSocio.add(jbGuardarSocio);
+
+        // --- 4. Lógica de los Componentes ---
+
+        // Lógica de Radio Buttons
+        ActionListener radioListener = new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (rbDocente.isSelected()) {
+                        jlCarreraAsignatura.setText("Asignatura:");
+                        jtfDiasPrestamo.setText("5");
+                    } else if (rbAlumno.isSelected()) {
+                        jlCarreraAsignatura.setText("Carrera:");
+                        jtfDiasPrestamo.setText("20");
+                    }
+                }
+            };
+        rbDocente.addActionListener(radioListener);
+        rbAlumno.addActionListener(radioListener);
+
+        // Lógica del Botón Guardar
+        jbGuardarSocio.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent evt) {
+                    // Llama a la logica del metodo
+                }
+
+                public void mouseEntered(MouseEvent evt) {
+                    jbGuardarSocio.jBotonMouseEntered(evt); 
+                }
+
+                public void mouseExited(MouseEvent evt) {
+                    jbGuardarSocio.jBotonMouseExited(evt);
+                }
+            });
+    }
+
 }
